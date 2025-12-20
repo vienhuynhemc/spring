a249822305ce41acb4c7891d1839e6dc9d603f82
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-12-20 22:23:03 +0700

    Add initial Spring Boot single datasource project
    Set up a new Spring Boot project with a single datasource configuration, including Maven wrapper, project structure, dependencies for JPA, WebMVC, PostgreSQL, Lombok, and test starters. Added basic application and test classes, configuration files, and Spotless code formatting setup.

---
+++ spring-batch/single-datasource/src/main/java/com/vienhuynhemc/single_datasource/SingleDatasourceApplication.java
@@ -0,0 +1,13 @@
+/* vienhuynhemc */
+package com.vienhuynhemc.single_datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleDatasourceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SingleDatasourceApplication.class, args);
+  }
+}
