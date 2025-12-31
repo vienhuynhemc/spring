63e1623f723830bf923e25af6db666e41563dc5d
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-12-31 23:58:22 +0700

    Initialize Spring Boot Kafka session-timeout project
    Add initial project structure for a Spring Boot application with Kafka integration. Includes Maven configuration, application entry point, basic test, and supporting files for build and IDE environments.

---
+++ spring-for-apache-kafka/session-timeout/src/main/java/com/vienhuynhemc/session_timeout/SessionTimeoutApplication.java
@@ -0,0 +1,13 @@
+package com.vienhuynhemc.session_timeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionTimeoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionTimeoutApplication.class, args);
+	}
+
+}
---
+++ spring-for-apache-kafka/session-timeout/src/test/java/com/vienhuynhemc/session_timeout/SessionTimeoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vienhuynhemc.session_timeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionTimeoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
