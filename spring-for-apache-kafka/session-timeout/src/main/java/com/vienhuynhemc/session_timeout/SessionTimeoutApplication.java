203e6aa16ee0e49ac7e41f04a0a46a05c9fea4c0
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2026-01-01 22:59:47 +0700

    Add Kafka configuration and apply code formatting
    Introduced KafkaConfig to set up KafkaAdmin and topic creation for 'session_timeout'. Updated application and test files with license headers and consistent formatting. Enhanced pom.xml with Spotless plugin for code style enforcement and formatting rules.

--- spring-for-apache-kafka/session-timeout/src/main/java/com/vienhuynhemc/session_timeout/SessionTimeoutApplication.java
+++ spring-for-apache-kafka/session-timeout/src/main/java/com/vienhuynhemc/session_timeout/SessionTimeoutApplication.java
@@ -1,3 +1,4 @@
+/* vienhuynhemc */
 package com.vienhuynhemc.session_timeout;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SessionTimeoutApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SessionTimeoutApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SessionTimeoutApplication.class, args);
+  }
 }
--- spring-for-apache-kafka/session-timeout/src/test/java/com/vienhuynhemc/session_timeout/SessionTimeoutApplicationTests.java
+++ spring-for-apache-kafka/session-timeout/src/test/java/com/vienhuynhemc/session_timeout/SessionTimeoutApplicationTests.java
@@ -1,3 +1,4 @@
+/* vienhuynhemc */
 package com.vienhuynhemc.session_timeout;
 
 import org.junit.jupiter.api.Test;
@@ -6,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SessionTimeoutApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
