3a349c1419741c3e4b627da3a5cb83237e68882a
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2026-01-23 23:54:49 +0700

    Set Kafka listener to manual ack mode and disable auto-commit
    Configured the Kafka listener container to use manual acknowledgment mode in KafkaConfig.java and set 'enable-auto-commit' to false in application.yaml. This change allows for explicit control over message acknowledgment and offset commits.

--- spring-for-apache-kafka/consume-event/src/main/java/com/vienhuynhemc/consume_event/config/KafkaConfig.java
+++ spring-for-apache-kafka/consume-event/src/main/java/com/vienhuynhemc/consume_event/config/KafkaConfig.java
@@ -13,6 +13,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
@@ -38,6 +39,7 @@ public class KafkaConfig {
     final ConsumerFactory<String, OrderStatusUpdatedEvent> consumerFactory = new DefaultKafkaConsumerFactory<>(props);
 
     factory.setConsumerFactory(consumerFactory);
+    factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
 
     return factory;
   }
